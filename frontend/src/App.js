a5c9d22526453550f75babc52d42892905e61fa8
Dulanga Niroshan <dulanganiroshan721@gmail.com>
2024-04-10 12:55:48 +0530

    Header & Footer Created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,19 @@
-import React from "react";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
-  return <h1>Welcome to Proshop</h1>;
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to Proshop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
