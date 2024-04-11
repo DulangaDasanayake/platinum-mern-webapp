fd3aa5c4d11c1a63d8b00dff3102888fb76662d3
Dulanga Niroshan <dulanganiroshan721@gmail.com>
2024-04-11 12:35:41 +0530

    Connect With Mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import connectDB from "./config/db.js";
 import products from "./data/products.js";
 const port = process.env.PORT || 5000;
+
+connectDB(); //Connect to MongoDB
 
 const app = express();
 
