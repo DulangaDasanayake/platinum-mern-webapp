e64898b56bc06ae498ba9b5ee5a577aa4375be7c
Dulanga Niroshan <dulanganiroshan721@gmail.com>
2024-04-12 03:08:08 +0530

    Custom Error Middleware

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -18,8 +18,10 @@ router.get(
 
     if (product) {
       return res.json(product);
+    } else {
+      res.status(404);
+      throw new Error("Resource not found");
     }
-    res.status(404).json({ message: "Product not found" });
   })
 );
 
