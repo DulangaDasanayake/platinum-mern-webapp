2d5ef3f5639cb0c0d851bda34de6ef3889044a4f
Dulanga Niroshan <dulanganiroshan721@gmail.com>
2024-04-11 10:33:17 +0530

    Environment Variables

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 import products from "./data/products.js";
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 const app = express();
 
