aa78303fd3be617d96e1454a0d10c380f116b5e5
Dulanga Niroshan <dulanganiroshan721@gmail.com>
2024-04-13 16:19:17 +0530

    Get Product Details Endpoint Challenge

--- frontend/src/slices/productApiSlice.js
+++ frontend/src/slices/productApiSlice.js
@@ -9,7 +9,13 @@ export const productApiSlice = apiSlice.injectEndpoints({
       }),
       keepUnusedDataFor: 5,
     }),
+    getProductDetails: builder.query({
+      query: (productId) => ({
+        url: `${PRODUCTS_URL}/${productId}`,
+      }),
+      keepUnusedDataFor: 5,
+    }),
   }),
 });
 
-export const { useGetProductsQuery } = productApiSlice;
+export const { useGetProductsQuery, useGetProductDetailsQuery } = productApiSlice;
