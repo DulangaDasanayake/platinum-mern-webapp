5f18d65f796e423d481ec758f1acc0b0c3dec0c3
Dulanga Niroshan <dulanganiroshan721@gmail.com>
2024-04-15 01:22:38 +0530

    JWT HTTP Only Cookie

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 import User from "../models/userModel.js";
+import jwt from "jsonwebtoken";
 
 // @desc Auth user & get token
 // @route GET/api/users/login
@@ -10,6 +11,18 @@ const authUser = asyncHandler(async (req, res) => {
   const user = await User.findOne({ email });
 
   if (user && (await user.matchPassword(password))) {
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "30d",
+    });
+
+    //set JWT as HTTP-only cookie
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== "development",
+      sameSite: "strict",
+      maxAge: 30 * 24 * 60 * 60 * 1000, //30 Days
+    });
+
     res.json({
       _id: user._id,
       name: user.name,
