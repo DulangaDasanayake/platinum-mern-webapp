224a16dee060c8636a8ebb9ebd83e88fd82a424e
Dulanga Niroshan <dulanganiroshan721@gmail.com>
2024-04-15 03:06:39 +0530

    Logout User & Clear Cookie

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -46,7 +46,12 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route POST/api/users/logout
 // @access Private
 const logoutUser = asyncHandler(async (req, res) => {
-  res.send("logout user");
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+
+  res.status(200).json({ message: "Logged out successfully" });
 });
 
 // @desc Get user profile
