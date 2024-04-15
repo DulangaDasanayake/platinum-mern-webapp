872f46947e36f88e4ba16fb099dfaa74dc84e322
Dulanga Niroshan <dulanganiroshan721@gmail.com>
2024-04-15 21:46:45 +0530

    Login Screen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -16,6 +16,7 @@ import reportWebVitals from "./reportWebVitals";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -23,6 +24,7 @@ const router = createBrowserRouter(
       <Route index={true} path="/" element={<HomeScreen />} />
       <Route path="/product/:id" element={<ProductScreen />} />
       <Route path="/cart/" element={<CartScreen />} />
+      <Route path="/login" element={<LoginScreen />} />
     </Route>
   )
 );
---
+++ frontend/src/screens/LoginScreen.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { Form, Button, Row, Col } from "react-bootstrap";
+import FormContainer from "../components/FormContainer";
+
+const LoginScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("submit");
+  };
+
+  return (
+    <FormContainer>
+      <h1>Sign In</h1>
+
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId="email" className="my-3">
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId="password" className="my-3">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Enter password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Button type="submit" variant="primary" className="mt-2">
+          Sign In
+        </Button>
+      </Form>
+
+      <Row className="py-3">
+        <Col>
+          New Customer?<Link to="/register"></Link>
+        </Col>
+      </Row>
+    </FormContainer>
+  );
+};
+
+export default LoginScreen;
