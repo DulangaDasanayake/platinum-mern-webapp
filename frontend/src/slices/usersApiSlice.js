a0eaaa4de6600b83e67cbc8819dbec2e5283e670
Dulanga Niroshan <dulanganiroshan721@gmail.com>
2024-04-15 21:14:50 +0530

    Auth & User API Slice

---
+++ frontend/src/slices/usersApiSlice.js
@@ -0,0 +1,16 @@
+import { USERS_URL } from "../constants";
+import { apiSlice } from "./apiSlice";
+
+export const usersApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (data) => ({
+        url: USERS_URL / auth,
+        method: "POST",
+        body: data,
+      }),
+    }),
+  }),
+});
+
+export const { useLoginMutation } = usersApiSlice;
