e2abae4175bbc37981e981cc0b3500a89f6615ba
Dulanga Niroshan <dulanganiroshan721@gmail.com>
2024-04-18 11:19:25 +0530

    Paypal Setup & Order Paid

--- backend/server.js
+++ backend/server.js
@@ -18,15 +18,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //Cookie parser middleware
-app.use(cookieParser())
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
-  res.send("API is running..."); 
+  res.send("API is running...");
 });
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
+
+app.get("/api/config/paypal", (req, res) =>
+  res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
+);
 
 app.use(notFound);
 app.use(errorHandler);
