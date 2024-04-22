627f6c408faad5f649b42316a1a38b8bf0e8406d
Dulanga Niroshan <dulanganiroshan721@gmail.com>
2024-04-22 19:29:56 +0530

    Styles & Comments Edit

--- frontend/src/components/ProductCarousel.jsx
+++ frontend/src/components/ProductCarousel.jsx
@@ -12,15 +12,24 @@ const ProductCarousel = () => {
   ) : error ? (
     <Message variant="danger">{error}</Message>
   ) : (
-    <Carousel pause="hover" className="bg-primary mb-4" style={{ width: '1280px' }}>
+    <Carousel
+      pause="hover"
+      className="bg-primary mb-4"
+    >
       {products.map((product) => (
         <Carousel.Item key={product._id}>
           <Link to={`/product/${product._id}`}>
-            <Image src={product.image} alt={product.name} fluid />
+            <Image
+              src={product.image}
+              alt={product.name}
+              fluid
+              style={{ width: "1296px" }}
+            />
             <Carousel.Caption className="carousel-caption">
               <h2>
                 {product.name} (${product.price})
               </h2>
+              <h5>{product.description}</h5>
             </Carousel.Caption>
           </Link>
         </Carousel.Item>
