be384395a024f1674234e28c181bee71837649a8
Dulanga Niroshan <dulanganiroshan721@gmail.com>
2024-04-23 04:14:38 +0530

    Error Fixed

--- frontend/src/screens/admin/UserListScreen.jsx
+++ frontend/src/screens/admin/UserListScreen.jsx
@@ -3,7 +3,7 @@ import { Table, Button } from "react-bootstrap";
 import { FaTimes, FaTrash, FaEdit, FaCheck } from "react-icons/fa";
 import Message from "../../components/Message";
 import Loader from "../../components/Loader";
-import { toast, Toast } from "react-toastify";
+import { toast } from "react-toastify";
 import {
   useGetUsersQuery,
   useDeleteUserMutation,
