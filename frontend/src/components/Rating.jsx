c12285d5eea0d626e83999f61676a2a3d4d1893d
Dulanga Niroshan <dulanganiroshan721@gmail.com>
2024-04-23 06:56:15 +0530

    Star Color Changed

--- frontend/src/components/Rating.jsx
+++ frontend/src/components/Rating.jsx
@@ -1,55 +1,60 @@
 import { FaStar, FaStarHalfAlt, FaRegStar } from "react-icons/fa";
 
 const Rating = ({ value, text }) => {
+  // Define a style object to set the color of the stars
+  const starStyle = {
+    color: "#d39d03", // Change this color to your desired color
+  };
+
   return (
     <div className="rating">
       <span>
         {value >= 1 ? (
-          <FaStar />
+          <FaStar style={starStyle} />
         ) : value >= 0.5 ? (
-          <FaStarHalfAlt />
+          <FaStarHalfAlt style={starStyle} />
         ) : (
-          <FaRegStar />
+          <FaRegStar style={starStyle} />
         )}
       </span>
 
       <span>
         {value >= 2 ? (
-          <FaStar />
+          <FaStar style={starStyle} />
         ) : value >= 1.5 ? (
-          <FaStarHalfAlt />
+          <FaStarHalfAlt style={starStyle} />
         ) : (
-          <FaRegStar />
+          <FaRegStar style={starStyle} />
         )}
       </span>
 
       <span>
         {value >= 3 ? (
-          <FaStar />
+          <FaStar style={starStyle} />
         ) : value >= 2.5 ? (
-          <FaStarHalfAlt />
+          <FaStarHalfAlt style={starStyle} />
         ) : (
-          <FaRegStar />
+          <FaRegStar style={starStyle} />
         )}
       </span>
 
       <span>
         {value >= 4 ? (
-          <FaStar />
+          <FaStar style={starStyle} />
         ) : value >= 3.5 ? (
-          <FaStarHalfAlt />
+          <FaStarHalfAlt style={starStyle} />
         ) : (
-          <FaRegStar />
+          <FaRegStar style={starStyle} />
         )}
       </span>
 
       <span>
         {value >= 5 ? (
-          <FaStar />
+          <FaStar style={starStyle} />
         ) : value >= 4.5 ? (
-          <FaStarHalfAlt />
+          <FaStarHalfAlt style={starStyle} />
         ) : (
-          <FaRegStar />
+          <FaRegStar style={starStyle} />
         )}
       </span>
       <span className="rating-text">{text && text}</span>
