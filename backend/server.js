3ae6fec8d0f3ed461edd71fc264228180019114b
Dulanga Niroshan <dulanganiroshan721@gmail.com>
2024-04-24 18:28:26 +0530

    Update Server.js

--- backend/server.js
+++ backend/server.js
@@ -4,23 +4,20 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 dotenv.config();
 import connectDB from "./config/db.js";
-import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 const port = process.env.PORT || 5000;
 
-connectDB(); //Connect to MongoDB
+connectDB();
 
 const app = express();
 
-//body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-//Cookie parser middleware
 app.use(cookieParser());
 
 app.use("/api/products", productRoutes);
@@ -32,24 +29,25 @@ app.get("/api/config/paypal", (req, res) =>
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
 );
 
-const __dirname = path.resolve(); //Set __dirname to current directory
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
-
 if (process.env.NODE_ENV === "production") {
-  //set static folder
+  const __dirname = path.resolve();
+  app.use("/uploads", express.static("/var/data/uploads"));
   app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-  //any route that is not api will be redirected to index.html
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
   );
 } else {
+  const __dirname = path.resolve();
+  app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
   app.get("/", (req, res) => {
-    res.send("API is running...");
+    res.send("API is running....");
   });
 }
 
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+);
