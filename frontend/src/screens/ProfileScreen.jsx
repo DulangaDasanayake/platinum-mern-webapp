a04b2895529717e572badbb8b1c1d8175517153f
Dulanga Niroshan <dulanganiroshan721@gmail.com>
2024-04-24 18:30:14 +0530

    Update ProfileScreen.jsx

--- frontend/src/screens/ProfileScreen.jsx
+++ frontend/src/screens/ProfileScreen.jsx
@@ -1,41 +1,39 @@
-import { useState, useEffect } from "react";
-import { Table, Form, Button, Row, Col } from "react-bootstrap";
-import { LinkContainer } from "react-router-bootstrap";
-import { useDispatch, useSelector } from "react-redux";
-import { toast } from "react-toastify";
-import Message from "../components/Message";
-import Loader from "../components/Loader";
-import { FaTimes } from "react-icons/fa";
-import { useProfileMutation } from "../slices/usersApiSlice";
-import { setCredentials } from "../slices/authSlice";
-import { useGetMyOrdersQuery } from "../slices/ordersApiSlice";
+import React, { useEffect, useState } from 'react';
+import { Table, Form, Button, Row, Col } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { FaTimes } from 'react-icons/fa';
+
+import { toast } from 'react-toastify';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
+import { useProfileMutation } from '../slices/usersApiSlice';
+import { useGetMyOrdersQuery } from '../slices/ordersApiSlice';
+import { setCredentials } from '../slices/authSlice';
 
 const ProfileScreen = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-
-  const dispatch = useDispatch();
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
 
   const { userInfo } = useSelector((state) => state.auth);
+
+  const { data: orders, isLoading, error } = useGetMyOrdersQuery();
 
   const [updateProfile, { isLoading: loadingUpdateProfile }] =
     useProfileMutation();
 
-  const { data: orders, isLoading, error } = useGetMyOrdersQuery();
-
   useEffect(() => {
-    if (userInfo) {
-      setName(userInfo.name);
-      setEmail(userInfo.email);
-    }
-  }, [userInfo, userInfo.name, userInfo.email]);
+    setName(userInfo.name);
+    setEmail(userInfo.email);
+  }, [userInfo.email, userInfo.name]);
 
+  const dispatch = useDispatch();
   const submitHandler = async (e) => {
     e.preventDefault();
     if (password !== confirmPassword) {
-      toast.error("Password do not match");
+      toast.error('Passwords do not match');
     } else {
       try {
         const res = await updateProfile({
@@ -44,8 +42,8 @@ const ProfileScreen = () => {
           email,
           password,
         }).unwrap();
-        dispatch(setCredentials(res));
-        toast.success("Profile update successfully");
+        dispatch(setCredentials({ ...res }));
+        toast.success('Profile updated successfully');
       } catch (err) {
         toast.error(err?.data?.message || err.error);
       }
@@ -58,63 +56,62 @@ const ProfileScreen = () => {
         <h2>User Profile</h2>
 
         <Form onSubmit={submitHandler}>
-          <Form.Group controlId="name" className="my-2">
+          <Form.Group className='my-2' controlId='name'>
             <Form.Label>Name</Form.Label>
             <Form.Control
-              type="name"
-              placeholder="Enter name"
+              type='name'
+              placeholder='Enter name'
               value={name}
               onChange={(e) => setName(e.target.value)}
             ></Form.Control>
           </Form.Group>
 
-          <Form.Group controlId="email" className="my-2">
+          <Form.Group className='my-2' controlId='email'>
             <Form.Label>Email Address</Form.Label>
             <Form.Control
-              type="email"
-              placeholder="Enter email"
+              type='email'
+              placeholder='Enter email'
               value={email}
               onChange={(e) => setEmail(e.target.value)}
             ></Form.Control>
           </Form.Group>
 
-          <Form.Group controlId="password" className="my-2">
+          <Form.Group className='my-2' controlId='password'>
             <Form.Label>Password</Form.Label>
             <Form.Control
-              type="password"
-              placeholder="Enter password"
+              type='password'
+              placeholder='Enter password'
               value={password}
               onChange={(e) => setPassword(e.target.value)}
             ></Form.Control>
           </Form.Group>
 
-          <Form.Group controlId="confirmPassword" className="my-2">
+          <Form.Group className='my-2' controlId='confirmPassword'>
             <Form.Label>Confirm Password</Form.Label>
             <Form.Control
-              type="password"
-              placeholder="Confirm password"
+              type='password'
+              placeholder='Confirm password'
               value={confirmPassword}
               onChange={(e) => setConfirmPassword(e.target.value)}
             ></Form.Control>
           </Form.Group>
 
-          <Button type="submit" variant="primary" className="my-2">
+          <Button type='submit' variant='primary'>
             Update
           </Button>
           {loadingUpdateProfile && <Loader />}
         </Form>
       </Col>
-
       <Col md={9}>
         <h2>My Orders</h2>
         {isLoading ? (
           <Loader />
         ) : error ? (
-          <Message variant="danger">
+          <Message variant='danger'>
             {error?.data?.message || error.error}
           </Message>
         ) : (
-          <Table striped hover responsive className="table-sm">
+          <Table striped hover responsive className='table-sm'>
             <thead>
               <tr>
                 <th>ID</th>
@@ -129,25 +126,25 @@ const ProfileScreen = () => {
               {orders.map((order) => (
                 <tr key={order._id}>
                   <td>{order._id}</td>
-                  <td>{order.createdAt?.substring(0, 10)}</td>
-                  <td>${order.totalPrice}</td>
+                  <td>{order.createdAt.substring(0, 10)}</td>
+                  <td>{order.totalPrice}</td>
                   <td>
                     {order.isPaid ? (
-                      order.paidAt?.substring(0, 10)
+                      order.paidAt.substring(0, 10)
                     ) : (
-                      <FaTimes style={{ color: "red" }} />
+                      <FaTimes style={{ color: 'red' }} />
                     )}
                   </td>
                   <td>
                     {order.isDelivered ? (
-                      order.deliveredAt?.substring(0, 10)
+                      order.deliveredAt.substring(0, 10)
                     ) : (
-                      <FaTimes style={{ color: "red" }} />
+                      <FaTimes style={{ color: 'red' }} />
                     )}
                   </td>
                   <td>
                     <LinkContainer to={`/order/${order._id}`}>
-                      <Button className="btn-sm" variant="light">
+                      <Button className='btn-sm' variant='light'>
                         Details
                       </Button>
                     </LinkContainer>
