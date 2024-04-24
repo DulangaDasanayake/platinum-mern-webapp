8aaf25b45f234569e61ae77a891d36f916493692
Dulanga Niroshan <dulanganiroshan721@gmail.com>
2024-04-24 18:06:51 +0530

    Small Changes

--- frontend/src/components/ProductCarousel.jsx
+++ frontend/src/components/ProductCarousel.jsx
@@ -1,35 +1,23 @@
-import { Link } from "react-router-dom";
-import { Carousel, Image } from "react-bootstrap";
-import Loader from "./Loader";
-import Message from "./Message";
-import { useGetTopProductsQuery } from "../slices/productsApiSlice";
+import { Link } from 'react-router-dom';
+import { Carousel, Image } from 'react-bootstrap';
+import Message from './Message';
+import { useGetTopProductsQuery } from '../slices/productsApiSlice';
 
 const ProductCarousel = () => {
   const { data: products, isLoading, error } = useGetTopProductsQuery();
 
-  return isLoading ? (
-    <Loader />
-  ) : error ? (
-    <Message variant="danger">{error}</Message>
+  return isLoading ? null : error ? (
+    <Message variant='danger'>{error?.data?.message || error.error}</Message>
   ) : (
-    <Carousel
-      pause="hover"
-      className="bg-primary mb-4"
-    >
+    <Carousel pause='hover' className='bg-primary mb-4'>
       {products.map((product) => (
         <Carousel.Item key={product._id}>
           <Link to={`/product/${product._id}`}>
-            <Image
-              src={product.image}
-              alt={product.name}
-              fluid
-              style={{ width: "1296px" }}
-            />
-            <Carousel.Caption className="carousel-caption">
-              <h2>
+            <Image src={product.image} alt={product.name} fluid />
+            <Carousel.Caption className='carousel-caption'>
+              <h2 className='text-white text-right'>
                 {product.name} (${product.price})
               </h2>
-              <h5>{product.description}</h5>
             </Carousel.Caption>
           </Link>
         </Carousel.Item>
