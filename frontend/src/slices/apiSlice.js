89aab8d2cdf30b318ba55cb651b73939b6b74e5e
Dulanga Niroshan <dulanganiroshan721@gmail.com>
2024-04-24 19:54:42 +0530

    handle 401 response

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,10 +1,23 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { BASE_URL } from "../constants";
+import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
+import { BASE_URL } from '../constants';
 
-const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL });
+import { logout } from './authSlice'; // Import the logout action
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: BASE_URL,
+});
+
+async function baseQueryWithAuth(args, api, extra) {
+  const result = await baseQuery(args, api, extra);
+  // Dispatch the logout action on 401.
+  if (result.error && result.error.status === 401) {
+    api.dispatch(logout());
+  }
+  return result;
+}
 
 export const apiSlice = createApi({
-  baseQuery,
-  tagTypes: ["Product", "Order", "User"],
+  baseQuery: baseQueryWithAuth, // Use the customized baseQuery
+  tagTypes: ['Product', 'Order', 'User'],
   endpoints: (builder) => ({}),
 });
