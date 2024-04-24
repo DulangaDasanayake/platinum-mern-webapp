0406e521028cafc6871076907a12df3bc301355c
Dulanga Niroshan <dulanganiroshan721@gmail.com>
2024-04-24 18:00:20 +0530

    Finishing Touches

--- frontend/src/slices/authSlice.js
+++ frontend/src/slices/authSlice.js
@@ -13,6 +13,9 @@ const authSlice = createSlice({
     setCredentials: (state, action) => {
       state.userInfo = action.payload;
       localStorage.setItem("userInfo", JSON.stringify(action.payload));
+
+      const expirationTime = new Date().getTime() + 30 * 24 * 60 * 60 * 1000; // 30 days
+      localStorage.setItem('expirationTime', expirationTime);
     },
     logout: (state, action) => {
       state.userInfo = null;
