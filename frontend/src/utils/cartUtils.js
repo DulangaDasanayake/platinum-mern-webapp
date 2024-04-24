ebd39f5accfece9116860fb1a61f41bcad863b33
Dulanga Niroshan <dulanganiroshan721@gmail.com>
2024-04-24 19:51:16 +0530

    fix: passing string type to addDecimals

--- frontend/src/utils/cartUtils.js
+++ frontend/src/utils/cartUtils.js
@@ -2,26 +2,33 @@ export const addDecimals = (num) => {
   return (Math.round(num * 100) / 100).toFixed(2);
 };
 
+// NOTE: the code below has been changed from the course code to fix an issue
+// with type coercion of strings to numbers.
+// Our addDecimals function expects a number and returns a string, so it is not
+// correct to call it passing a string as the argument.
+
 export const updateCart = (state) => {
-  //calculate item price
-  state.itemsPrice = addDecimals(
-    state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+  // Calculate the items price
+  const itemsPrice = state.cartItems.reduce(
+    (acc, item) => acc + item.price * item.qty,
+    0
   );
+  state.itemsPrice = addDecimals(itemsPrice);
 
-  //calculate shipping price(if order is over $100 then free, else $10 shipping)
-  state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
+  // Calculate the shipping price
+  const shippingPrice = itemsPrice > 100 ? 0 : 10;
+  state.shippingPrice = addDecimals(shippingPrice);
 
-  //calculate tax price(15% tax)
-  state.taxPrice = addDecimals(Number((0.15 * state.itemsPrice).toFixed(2)));
+  // Calculate the tax price
+  const taxPrice = 0.15 * itemsPrice;
+  state.taxPrice = addDecimals(taxPrice);
 
-  //calculate total price
-  state.totalPrice = (
-    Number(state.itemsPrice) +
-    Number(state.shippingPrice) +
-    Number(state.taxPrice)
-  ).toFixed(2);
+  const totalPrice = itemsPrice + shippingPrice + taxPrice;
+  // Calculate the total price
+  state.totalPrice = addDecimals(totalPrice);
 
-  localStorage.setItem("cart", JSON.stringify(state));
+  // Save the cart to localStorage
+  localStorage.setItem('cart', JSON.stringify(state));
 
   return state;
 };
