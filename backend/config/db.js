bdddfba6498c3ca6f9c530cf75fce22a95226cea
Dulanga Niroshan <dulanganiroshan721@gmail.com>
2024-04-29 22:21:32 +0530

    db.js Update

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log(`MongoDB Connection Success: ${conn.connection.host}`);
   } catch (error) {
     console.log(`Error: ${error.message}`);
     process.exit(1);
