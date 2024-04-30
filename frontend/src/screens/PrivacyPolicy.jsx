2f56e29af676d327a30702efe1c2a56cca922b71
Dulanga Niroshan <dulanganiroshan721@gmail.com>
2024-05-01 03:29:28 +0530

    Privacy policy update

--- frontend/src/screens/PrivacyPolicy.jsx
+++ frontend/src/screens/PrivacyPolicy.jsx
@@ -1,4 +1,3 @@
-jsx;
 import React from 'react';
 
 const PrivacyPolicy = () => {
@@ -10,106 +9,160 @@ const PrivacyPolicy = () => {
         priorities. This Privacy Policy details how we collect, use, and protect
         your information when you use our e-commerce web application.
       </p>
-      <p>Security: Do not share your login information with anyone.</p>
-      <p>1. Information We Collect:</p>
-      <p>
-        a. Personal Information: When you create an account, place an order, or
-        interact with our platform, we may collect personal details such as your
-        name, email address, shipping address, billing information, and phone
-        number.
-      </p>
-      <p>
-        b. Payment Information: We may collect payment details such as credit
-        card numbers or other financial information when you make a purchase on
-        our platform. This information is securely processed by our payment
-        service providers, and we do not store sensitive payment data on our
-        servers.
-      </p>
-      <p>
-        c. Device and Usage Information: We automatically collect certain
-        information about your device and usage of our platform, including your
-        IP address, browser type, operating system, and pages visited. This
-        helps us improve our services and enhance your browsing experience.
-      </p>
-      <p>2. How We Use Your Information:</p>
-      <p>
-        a. Order Processing: We use the information you provide to process your
-        orders, fulfill your purchases, and deliver products to your specified
-        address.
-      </p>
-      <p>
-        b. Customer Support: Your information helps us respond to your
-        inquiries, provide customer support, and address any issues or concerns
-        you may have.
-      </p>
-      <p>
-        c. Personalization: We may use your information to personalize your
-        shopping experience, recommend products, and tailor promotional offers
-        based on your preferences and browsing history.
-      </p>
-      <p>
-        d. Marketing Communications: With your consent, we may send you
-        marketing communications, newsletters, or promotional offers via email
-        or other channels. You can opt-out of these communications at any time.
-      </p>
-      <p>3. Sharing of Information:</p>
-      <p>
-        a. Third-Party Service Providers: We may share your information with
-        trusted third-party service providers who assist us in processing
-        payments, delivering orders, analyzing data, or conducting marketing
-        activities on our behalf.
-      </p>
-      <p>
-        b. Legal Compliance: We may disclose your information when required by
-        law or in response to legal requests, such as to comply with a subpoena
-        or other legal process, or to protect our rights, property, or safety,
-        as well as the rights, property, or safety of others.
-      </p>
-      <p>4. Data Security:</p>
-      <p>
-        We take the security of your information seriously and implement
-        appropriate technical and organizational measures to protect it from
-        unauthorized access, disclosure, alteration, or destruction. However, no
-        method of transmission over the internet or electronic storage is 100%
-        secure, and we cannot guarantee absolute security.
-      </p>
-      <p>5. Your Rights:</p>
-      <p>
-        a. Access and Correction: You have the right to access and update your
-        personal information. You can do this by logging into your account or
-        contacting us directly.
-      </p>
-      <p>
-        b. Data Portability: Upon request, we will provide you with a copy of
-        your personal information in a structured, commonly used, and
-        machine-readable format.
-      </p>
-      <p>
-        c. Data Retention: We will retain your personal information for as long
-        as necessary to fulfill the purposes outlined in this Privacy Policy,
-        unless a longer retention period is required or permitted by law.
-      </p>
-      <p>6. Children's Privacy:</p>
-      <p>
-        Nova Mart is not directed at children under the age of 18, and we do not
-        knowingly collect personal information from individuals under 18. If you
-        are a parent or guardian and believe that your child has provided us
-        with personal information, please contact us, and we will promptly
-        delete such information from our records.
-      </p>
-      <p>7. Changes to This Privacy Policy:</p>
-      <p>
-        We reserve the right to update or modify this Privacy Policy at any
-        time. Any changes will be effective immediately upon posting the revised
-        policy on our website. We encourage you to review this Privacy Policy
-        periodically for any updates.
-      </p>
-      <p>8. Contact Us:</p>
-      <p>
-        If you have any questions or concerns about our Privacy Policy or the
-        handling of your personal information, please contact us at{' '}
-        <a href='mailto:contact@novamart.com'>contact@novamart.com</a>.
-      </p>
+      <ol>
+        <li>
+          <p>Information We Collect:</p>
+          <ol>
+            <li>
+              <p>
+                Personal Information: When you create an account, place an
+                order, or interact with our platform, we may collect personal
+                details such as your name, email address, shipping address,
+                billing information, and phone number.
+              </p>
+            </li>
+            <li>
+              <p>
+                Payment Information: We may collect payment details such as
+                credit card numbers or other financial information when you
+                make a purchase on our platform. This information is securely
+                processed by our payment service providers, and we do not store
+                sensitive payment data on our servers.
+              </p>
+            </li>
+            <li>
+              <p>
+                Device and Usage Information: We automatically collect certain
+                information about your device and usage of our platform,
+                including your IP address, browser type, operating system, and
+                pages visited. This helps us improve our services and enhance
+                your browsing experience.
+              </p>
+            </li>
+          </ol>
+        </li>
+        <li>
+          <p>How We Use Your Information:</p>
+          <ol>
+            <li>
+              <p>
+                Order Processing: We use the information you provide to process
+                your orders, fulfill your purchases, and deliver products to
+                your specified address.
+              </p>
+            </li>
+            <li>
+              <p>
+                Customer Support: Your information helps us respond to your
+                inquiries, provide customer support, and address any issues or
+                concerns you may have.
+              </p>
+            </li>
+            <li>
+              <p>
+                Personalization: We may use your information to personalize
+                your shopping experience, recommend products, and tailor
+                promotional offers based on your preferences and browsing
+                history.
+              </p>
+            </li>
+            <li>
+              <p>
+                Marketing Communications: With your consent, we may send you
+                marketing communications, newsletters, or promotional offers
+                via email or other channels. You can opt-out of these
+                communications at any time.
+              </p>
+            </li>
+          </ol>
+        </li>
+        <li>
+          <p>Sharing of Information:</p>
+          <ol>
+            <li>
+              <p>
+                Third-Party Service Providers: We may share your information
+                with trusted third-party service providers who assist us in
+                processing payments, delivering orders, analyzing data, or
+                conducting marketing activities on our behalf.
+              </p>
+            </li>
+            <li>
+              <p>
+                Legal Compliance: We may disclose your information when required
+                by law or in response to legal requests, such as to comply with
+                a subpoena or other legal process, or to protect our rights,
+                property, or safety, as well as the rights, property, or safety
+                of others.
+              </p>
+            </li>
+          </ol>
+        </li>
+        <li>
+          <p>Data Security:</p>
+          <p>
+            We take the security of your information seriously and implement
+            appropriate technical and organizational measures to protect it
+            from unauthorized access, disclosure, alteration, or destruction.
+            However, no method of transmission over the internet or electronic
+            storage is 100% secure, and we cannot guarantee absolute security.
+          </p>
+        </li>
+        <li>
+          <p>Your Rights:</p>
+          <ol>
+            <li>
+              <p>
+                Access and Correction: You have the right to access and update
+                your personal information. You can do this by logging into your
+                account or contacting us directly.
+              </p>
+            </li>
+            <li>
+              <p>
+                Data Portability: Upon request, we will provide you with a copy
+                of your personal information in a structured, commonly used,
+                and machine-readable format.
+              </p>
+            </li>
+            <li>
+              <p>
+                Data Retention: We will retain your personal information for as
+                long as necessary to fulfill the purposes outlined in this
+                Privacy Policy unless a longer retention period is required or
+                permitted by law.
+              </p>
+            </li>
+          </ol>
+        </li>
+        <li>
+          <p>Children's Privacy:</p>
+          <p>
+            Nova Mart is not directed at children under the age of 18, and we
+            do not knowingly collect personal information from individuals under
+            18. If you are a parent or guardian and believe that your child has
+            provided us with personal information, please contact us, and we
+            will promptly delete such information from our records.
+          </p>
+        </li>
+        <li>
+          <p>Changes to This Privacy Policy:</p>
+          <p>
+            We reserve the right to update or modify this Privacy Policy at any
+            time. Any changes will be effective immediately upon posting the
+            revised policy on our website. We encourage you to review this
+            Privacy Policy periodically for any updates.
+          </p>
+        </li>
+        <li>
+          <p>Contact Us:</p>
+          <p>
+            If you have any questions or concerns about our Privacy Policy or
+            the handling of your personal information, please contact us at{' '}
+            <a href='mailto:contact@novamart.com'>contact@novamart.com</a>.
+          </p>
+        </li>
+      </ol>
       <p>
         By using our platform, you signify your acceptance of this Privacy
         Policy. If you do not agree to this policy, please do not use our
