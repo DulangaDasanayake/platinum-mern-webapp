65e0c7ebed68752f813fa46397f37d53235aa783
Dulanga Niroshan <dulanganiroshan721@gmail.com>
2024-05-05 06:13:03 +0530

    Footer.jsx update

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,15 +5,32 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer>
+    <footer className="bg-dark text-light py-4">
       <Container>
-        <Row>
-          <Col className='text-center py-3'>
+        <Row className="justify-content-center align-items-center">
+          <Col md={4} className='text-center'>
             <p>NovaMart &copy; {currentYear}</p>
-            <br />
-            <p>
-              <Link to='/privacypolicy'>Privacy Policy</Link>
-            </p>
+          </Col>
+          <Col md={4} className='text-center'>
+            <div className="d-flex flex-column">
+              <Link to='/privacypolicy' className="text-light mb-2">Privacy Policy</Link>
+              <Link to='/terms' className="text-light mb-2">Terms of Service</Link>
+              <Link to='/contact' className="text-light mb-2">Contact Us</Link>
+            </div>
+          </Col>
+          <Col md={4} className='text-center'>
+            <p>Connect with us:</p>
+            <ul className="list-inline mb-0">
+              <li className="list-inline-item">
+                <Link to="#" className="text-light">Facebook</Link>
+              </li>
+              <li className="list-inline-item">
+                <Link to="#" className="text-light">Twitter</Link>
+              </li>
+              <li className="list-inline-item">
+                <Link to="#" className="text-light">Instagram</Link>
+              </li>
+            </ul>
           </Col>
         </Row>
       </Container>
