b251f4f9db4d90a5dd93e5b0c52634ad73bec145
Dulanga Niroshan <dulanganiroshan721@gmail.com>
2024-05-05 06:25:56 +0530

    Footer.jsx update

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,30 +5,46 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="bg-dark text-light py-4">
+    <footer className='bg-dark text-light py-4'>
       <Container>
-        <Row className="justify-content-center align-items-center">
+        <Row className='justify-content-center align-items-center'>
           <Col md={4} className='text-center'>
-            <p>NovaMart &copy; {currentYear}</p>
+            <h5>NovaMart &copy; {currentYear}</h5>
+            <ul className='list-inline mb-0'>
+              <li className='list-inline-item'>
+                <Link to='/privacypolicy' className='text-light'>
+                  Privacy Policy
+                </Link>
+              </li>
+              <li className='list-inline-item'>
+                <Link to='/terms' className='text-light'>
+                  Terms of Service
+                </Link>
+              </li>
+              <li className='list-inline-item'>
+                <Link to='/contact' className='text-light'>
+                  Contact Us
+                </Link>
+              </li>
+            </ul>
           </Col>
           <Col md={4} className='text-center'>
-            <div className="d-flex flex-column">
-              <Link to='/privacypolicy' className="text-light mb-2">Privacy Policy</Link>
-              <Link to='/terms' className="text-light mb-2">Terms of Service</Link>
-              <Link to='/contact' className="text-light mb-2">Contact Us</Link>
-            </div>
-          </Col>
-          <Col md={4} className='text-center'>
-            <p>Connect with us:</p>
-            <ul className="list-inline mb-0">
-              <li className="list-inline-item">
-                <Link to="#" className="text-light">Facebook</Link>
+            <h5>Connect with us:</h5>
+            <ul className='list-inline mb-0'>
+              <li className='list-inline-item'>
+                <Link to='#' className='text-light'>
+                  Facebook
+                </Link>
               </li>
-              <li className="list-inline-item">
-                <Link to="#" className="text-light">Twitter</Link>
+              <li className='list-inline-item'>
+                <Link to='#' className='text-light'>
+                  Twitter
+                </Link>
               </li>
-              <li className="list-inline-item">
-                <Link to="#" className="text-light">Instagram</Link>
+              <li className='list-inline-item'>
+                <Link to='#' className='text-light'>
+                  Instagram
+                </Link>
               </li>
             </ul>
           </Col>
