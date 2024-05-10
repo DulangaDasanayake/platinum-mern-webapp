9b0b56b6c03f7ee4737e6c987c3561289601931e
Dulanga Niroshan <dulanganiroshan721@gmail.com>
2024-05-10 19:53:01 +0530

    Remove comments

--- frontend/src/utils/cartUtils.js
+++ frontend/src/utils/cartUtils.js
@@ -2,11 +2,6 @@ export const addDecimals = (num) => {
   return (Math.round(num * 100) / 100).toFixed(2);
 };
 
-// NOTE: the code below has been changed from the course code to fix an issue
-// with type coercion of strings to numbers.
-// Our addDecimals function expects a number and returns a string, so it is not
-// correct to call it passing a string as the argument.
-
 export const updateCart = (state) => {
   // Calculate the items price
   const itemsPrice = state.cartItems.reduce(
