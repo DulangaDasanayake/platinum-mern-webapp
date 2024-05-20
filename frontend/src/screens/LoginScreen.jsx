efebf2b3b9ff30dd73b4ab5899f89a5b3917c9a4
Dulanga Niroshan <dulanganiroshan721@gmail.com>
2024-05-20 21:34:24 +0530

    LoginScreen Color change

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -1,16 +1,16 @@
-import { useState, useEffect } from "react";
-import { Link, useLocation, useNavigate } from "react-router-dom";
-import { Form, Button, Row, Col } from "react-bootstrap";
-import { useDispatch, useSelector } from "react-redux";
-import FormContainer from "../components/FormContainer";
-import Loader from "../components/Loader";
-import { useLoginMutation } from "../slices/usersApiSlice";
-import { setCredentials } from "../slices/authSlice";
-import { toast } from "react-toastify";
+import { useState, useEffect } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { Form, Button, Row, Col } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import FormContainer from '../components/FormContainer';
+import Loader from '../components/Loader';
+import { useLoginMutation } from '../slices/usersApiSlice';
+import { setCredentials } from '../slices/authSlice';
+import { toast } from 'react-toastify';
 
 const LoginScreen = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -21,7 +21,7 @@ const LoginScreen = () => {
 
   const { search } = useLocation();
   const sp = new URLSearchParams(search);
-  const redirect = sp.get("redirect") || "/";
+  const redirect = sp.get('redirect') || '/';
 
   useEffect(() => {
     if (userInfo) {
@@ -45,30 +45,30 @@ const LoginScreen = () => {
       <h1>Sign In</h1>
 
       <Form onSubmit={submitHandler}>
-        <Form.Group controlId="email" className="my-3">
+        <Form.Group controlId='email' className='my-3'>
           <Form.Label>Email Address</Form.Label>
           <Form.Control
-            type="email"
-            placeholder="Enter email"
+            type='email'
+            placeholder='Enter email'
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           ></Form.Control>
         </Form.Group>
 
-        <Form.Group controlId="password" className="my-3">
+        <Form.Group controlId='password' className='my-3'>
           <Form.Label>Password</Form.Label>
           <Form.Control
-            type="password"
-            placeholder="Enter password"
+            type='password'
+            placeholder='Enter password'
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           ></Form.Control>
         </Form.Group>
 
         <Button
-          type="submit"
-          variant="primary"
-          className="mt-2"
+          type='submit'
+          variant='primary'
+          className='mt-2'
           disabled={isLoading}
         >
           Sign In
@@ -77,11 +77,14 @@ const LoginScreen = () => {
         {isLoading && <Loader />}
       </Form>
 
-      <Row className="py-3">
+      <Row className='py-3'>
         <Col>
           New Customer?
-          <Link to={redirect ? `/register?redirect=${redirect}` : "/register"}>
-            {" "}
+          <Link
+            to={redirect ? `/register?redirect=${redirect}` : '/register'}
+            style={{ color: 'black' }}
+          >
+            {' '}
             Register
           </Link>
         </Col>
