531fb9553f298e9704d6de01465786224159bdcf
Dulanga Niroshan <dulanganiroshan721@gmail.com>
2024-05-20 21:33:55 +0530

    RegisterScreen Color change

--- frontend/src/screens/RegisterScreen.jsx
+++ frontend/src/screens/RegisterScreen.jsx
@@ -1,18 +1,18 @@
-import { useState, useEffect } from "react";
-import { Link, useLocation, useNavigate } from "react-router-dom";
-import { Form, Button, Row, Col } from "react-bootstrap";
-import { useDispatch, useSelector } from "react-redux";
-import FormContainer from "../components/FormContainer";
-import Loader from "../components/Loader";
-import { useRegisterMutation } from "../slices/usersApiSlice";
-import { setCredentials } from "../slices/authSlice";
-import { toast } from "react-toastify";
+import { useState, useEffect } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { Form, Button, Row, Col } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import FormContainer from '../components/FormContainer';
+import Loader from '../components/Loader';
+import { useRegisterMutation } from '../slices/usersApiSlice';
+import { setCredentials } from '../slices/authSlice';
+import { toast } from 'react-toastify';
 
 const RegisterScreen = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -23,7 +23,7 @@ const RegisterScreen = () => {
 
   const { search } = useLocation();
   const sp = new URLSearchParams(search);
-  const redirect = sp.get("redirect") || "/";
+  const redirect = sp.get('redirect') || '/';
 
   useEffect(() => {
     if (userInfo) {
@@ -34,7 +34,7 @@ const RegisterScreen = () => {
   const submitHandler = async (e) => {
     e.preventDefault();
     if (password !== confirmPassword) {
-      toast.error("Password do not match");
+      toast.error('Password do not match');
       return;
     } else {
       try {
@@ -52,50 +52,50 @@ const RegisterScreen = () => {
       <h1>Sign Up</h1>
 
       <Form onSubmit={submitHandler}>
-        <Form.Group controlId="name" className="my-3">
+        <Form.Group controlId='name' className='my-3'>
           <Form.Label>Name</Form.Label>
           <Form.Control
-            type="text"
-            placeholder="Enter name"
+            type='text'
+            placeholder='Enter name'
             value={name}
             onChange={(e) => setName(e.target.value)}
           ></Form.Control>
         </Form.Group>
 
-        <Form.Group controlId="email" className="my-3">
+        <Form.Group controlId='email' className='my-3'>
           <Form.Label>Email Address</Form.Label>
           <Form.Control
-            type="email"
-            placeholder="Enter email"
+            type='email'
+            placeholder='Enter email'
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           ></Form.Control>
         </Form.Group>
 
-        <Form.Group controlId="password" className="my-3">
+        <Form.Group controlId='password' className='my-3'>
           <Form.Label>Password</Form.Label>
           <Form.Control
-            type="password"
-            placeholder="Enter password"
+            type='password'
+            placeholder='Enter password'
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           ></Form.Control>
         </Form.Group>
 
-        <Form.Group controlId="confirmPassword" className="my-3">
+        <Form.Group controlId='confirmPassword' className='my-3'>
           <Form.Label>Confirm Password</Form.Label>
           <Form.Control
-            type="password"
-            placeholder="Confirm password"
+            type='password'
+            placeholder='Confirm password'
             value={confirmPassword}
             onChange={(e) => setConfirmPassword(e.target.value)}
           ></Form.Control>
         </Form.Group>
 
         <Button
-          type="submit"
-          variant="primary"
-          className="mt-2"
+          type='submit'
+          variant='primary'
+          className='mt-2'
           disabled={isLoading}
         >
           Register
@@ -104,11 +104,14 @@ const RegisterScreen = () => {
         {isLoading && <Loader />}
       </Form>
 
-      <Row className="py-3">
+      <Row className='py-3'>
         <Col>
           Already have an account?
-          <Link to={redirect ? `/login?redirect=${redirect}` : "/login"}>
-            {" "}
+          <Link
+            to={redirect ? `/login?redirect=${redirect}` : '/login'}
+            style={{ color: 'black' }}
+          >
+            {' '}
             Login
           </Link>
         </Col>
