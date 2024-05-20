7acfd43355856939419e315a77a03ad15be51c33
Dulanga Niroshan <dulanganiroshan721@gmail.com>
2024-05-20 19:21:23 +0530

    Footer update

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,56 +1,107 @@
+import React from 'react';
+import { FaDiscord, FaFacebook, FaLinkedin, FaTwitter } from 'react-icons/fa';
 import { Container, Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import './footer.css';
 
 const Footer = () => {
-  const currentYear = new Date().getFullYear();
-
   return (
-    <footer className='bg-dark text-light py-4'>
-      <Container>
-        <Row className='justify-content-center align-items-center'>
-          <Col md={4} className='text-center'>
-            <h5>Platinum &copy; {currentYear}</h5>
-            <ul className='list-inline mb-0'>
-              <li className='list-inline-item'>
-                <Link to='/privacypolicy' className='text-light'>
-                  Privacy Policy
-                </Link>
-              </li>
-              <li className='list-inline-item'>
-                <Link to='/terms' className='text-light'>
-                  Terms of Service
-                </Link>
-              </li>
-              <li className='list-inline-item'>
-                <Link to='/contact' className='text-light'>
-                  Contact Us
-                </Link>
-              </li>
-            </ul>
+    <div className='footer'>
+      <Container className='sb__footer section__padding'>
+        <Row className='sb__footer-links'>
+          <Col className='sb__footer-links_div'>
+            <h4>WORKING HOURS</h4>
+            <Link to='/'>
+              <p>Mon-Fri : 09:30AM - 06:00PM </p>
+            </Link>
+            <Link to='/'>
+              <p>Sat : 9:30AM - 2:00PM </p>
+            </Link>
+            <Link to='/'>
+              <p>Sun : Close</p>
+            </Link>
           </Col>
-          <Col md={4} className='text-center'>
-            <h5>Connect with us:</h5>
-            <ul className='list-inline mb-0'>
-              <li className='list-inline-item'>
-                <Link to='#' className='text-light'>
-                  Facebook
-                </Link>
-              </li>
-              <li className='list-inline-item'>
-                <Link to='#' className='text-light'>
-                  Twitter
-                </Link>
-              </li>
-              <li className='list-inline-item'>
-                <Link to='#' className='text-light'>
-                  Instagram
-                </Link>
-              </li>
-            </ul>
+          <Col className='sb__footer-links_div'>
+            <h4>SHOWROOMS</h4>
+            <Link to='/'>
+              <p>Kandy: +94776584975</p>
+            </Link>
+            <Link to='/'>
+              <p>Colombo: +94776584975</p>
+            </Link>
+          </Col>
+          <Col className='sb__footer-links_div'>
+            <h4>PARTNERS</h4>
+            <a href='https://www.apple.com'>
+              <p>Apple Inc.</p>
+            </a>
+            <a href='https://www.dell.com'>
+              <p>Dell Computers</p>
+            </a>
+            <a href='https://www.hp.com'>
+              <p>Hewlett Packard</p>
+            </a>
+            <a href='https://www.lenovo.com'>
+              <p>Lenovo Technologies</p>
+            </a>
+          </Col>
+          <Col className='sb__footer-links_div'>
+            <h4>COMPANY</h4>
+            <Link to='/'>
+              <p>About Us</p>
+            </Link>
+            <Link to='/'>
+              <p>Contact Us</p>
+            </Link>
+          </Col>
+          <Col className='sb__footer-links_div'>
+            <h4>CONNECT WITH US</h4>
+            <div className='socialmedia gap-2 h-5 w-5'>
+              <p>
+                <a href='https://www.facebook.com'>
+                  <FaFacebook style={{ color: '#0037a9', fontSize: '2rem' }} />
+                </a>
+              </p>
+              <p>
+                <a href='https://www.twitter.com'>
+                  <FaTwitter style={{ color: '#42b7ff', fontSize: '2rem' }} />
+                </a>
+              </p>
+              <p>
+                <a href='https://www.linkedin.com'>
+                  <FaLinkedin style={{ color: '#0656df', fontSize: '2rem' }} />
+                </a>
+              </p>
+              <p>
+                <a href='https://www.discord.com'>
+                  <FaDiscord style={{ fontSize: '2rem' }} />
+                </a>
+              </p>
+            </div>
+          </Col>
+        </Row>
+        <hr />
+        <Row className='sb__footer-below'>
+          <Col className='sb__footer-copyright'>
+            <p>Platinum &copy; {new Date().getFullYear()}</p>
+          </Col>
+          <Col className='sb__footer-below-links text-end'>
+            <Link to='/'>
+              <p>Terms & Conditions</p>
+            </Link>
+            <Link to='/privacypolicy'>
+              <p>Privacy</p>
+            </Link>
+            <Link to='/'>
+              <p>Security</p>
+            </Link>
+            <Link to='/'>
+              <p>Cookies</p>
+            </Link>
           </Col>
         </Row>
       </Container>
-    </footer>
+    </div>
   );
 };
 
